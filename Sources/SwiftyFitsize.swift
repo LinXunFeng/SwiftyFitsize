019eee80a79f582cca1f1513cf822de87c71aafa
LinXunFeng <598600855@qq.com>
2018-11-07 17:42:57 +0800

    ~ : 针对 iPad 适配后的大小进行缩放指定倍数;  ≈ : 无视设备类型，不进行缩放

--- Sources/SwiftyFitsize.swift
+++ Sources/SwiftyFitsize.swift
@@ -8,31 +8,78 @@
 
 import UIKit
 
+/// 
+@objc public enum SwiftyFitType: Int, RawRepresentable {
+    /// Original Value
+    case none = 0
+    /// ~
+    case flexible = 1
+    /// ≈
+    case force = 2
+}
+
 fileprivate let ScreenW = UIScreen.main.bounds.width
 
 public final class SwiftyFitsize {
     static let shared = SwiftyFitsize()
     private init() { }
     
-    // default reference width 375
+    /// 默认参照宽度
     private var referenceW: CGFloat = 375
+    /// 默认 iPad 适配缩放倍数 (0 , 1]
+    private var iPadFitMultiple: CGFloat = 0.5
     
-    public static func reference(width: CGFloat) {
+    /// 设置参照的相关参数
+    ///
+    /// - Parameters:
+    ///   - width: 参照的宽度
+    ///   - iPadFitMultiple: iPad 在适配后所得值的倍数 (0 , 1]
+    public static func reference(
+        width: CGFloat,
+        iPadFitMultiple: CGFloat
+    ) {
         SwiftyFitsize.shared.referenceW = width
+        SwiftyFitsize.shared.iPadFitMultiple =
+            (iPadFitMultiple > 1 || iPadFitMultiple < 0) ? 1 : iPadFitMultiple
     }
     
-    fileprivate func fitSize(_ v: CGFloat) -> CGFloat {
-        return ScreenW / referenceW * v
+    fileprivate func fitNumberSize(
+        _ value: CGFloat,
+        fitType: SwiftyFitType
+    ) -> CGFloat {
+        switch fitType {
+        case .none: return value
+        case .flexible:
+            return ScreenW / referenceW * value *
+                (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiom.pad
+                    ? SwiftyFitsize.shared.iPadFitMultiple : 1)
+        case .force: return ScreenW / referenceW * value
+        }
+    }
+    
+    fileprivate func fitFontSize(
+        _ font : UIFont,
+        type: SwiftyFitType
+    ) -> UIFont {
+        switch type {
+        case .flexible: return font~
+        case .force: return font≈
+        default: return font
+        }
     }
 }
 
 // MARK:- SwiftyFitsize
+/*
+ * ~ : 当设备为iPad时，适配后的 value 会再乘上 iPadFitMultiple
+ * ≈ : 强制适配，不论是iPhone还是iPad 都不会乘上 iPadFitMultiple
+ */
+
+// MARK: operator ~
 postfix operator ~
-
 public postfix func ~ (value: CGFloat) -> CGFloat {
-    return SwiftyFitsize.shared.fitSize(value)
+    return SwiftyFitsize.shared.fitNumberSize(value, fitType: .flexible)
 }
-
 public postfix func ~ (font: UIFont) -> UIFont {
     return UIFont(name: font.fontName, size: font.pointSize~) ?? font
 }
@@ -71,41 +118,98 @@ public postfix func ~ (value: UIEdgeInsets) -> UIEdgeInsets {
     )
 }
 
+// MARK: operator ≈
+postfix operator ≈
+public postfix func ≈ (value: CGFloat) -> CGFloat {
+    return SwiftyFitsize.shared.fitNumberSize(value, fitType: .force)
+}
+public postfix func ≈ (font: UIFont) -> UIFont {
+    return UIFont(name: font.fontName, size: font.pointSize~) ?? font
+}
+public postfix func ≈ (value: Int) -> CGFloat {
+    return CGFloat(value)≈
+}
+public postfix func ≈ (value: Float) -> CGFloat {
+    return CGFloat(value)≈
+}
+public postfix func ≈ (value: CGPoint) -> CGPoint {
+    return CGPoint(
+        x: value.x≈,
+        y: value.y≈
+    )
+}
+public postfix func ≈ (value: CGSize) -> CGSize {
+    return CGSize(
+        width: value.width≈,
+        height: value.height≈
+    )
+}
+public postfix func ≈ (value: CGRect) -> CGRect {
+    return CGRect(
+        x: value.origin.x≈,
+        y: value.origin.y≈,
+        width: value.size.width≈,
+        height: value.size.height≈
+    )
+}
+public postfix func ≈ (value: UIEdgeInsets) -> UIEdgeInsets {
+    return UIEdgeInsets(
+        top: value.top≈,
+        left: value.left≈,
+        bottom: value.bottom≈,
+        right: value.right≈
+    )
+}
 
 // MARK:- Xib/Storyboard
 public extension NSLayoutConstraint {
-    @IBInspectable private(set) var swiftyFitsize : Bool {
-        get { return true }
+    @IBInspectable var swiftyFitType: Int {
+        get { return SwiftyFitType.none.rawValue }
         set {
-            if newValue { constant = constant~ }
+            guard let type = SwiftyFitType.init(rawValue: newValue) else { return }
+            constant =  SwiftyFitsize.shared.fitNumberSize(constant, fitType: type)
+        }
+    }
+}
+public extension UILabel {
+    @IBInspectable var fontFitType: Int {
+        get { return SwiftyFitType.none.rawValue }
+        set {
+            guard let type = SwiftyFitType.init(rawValue: newValue) else { return }
+            guard let xfont = font else { return }
+            self.font =  SwiftyFitsize.shared.fitFontSize(xfont, type: type)
+        }
+    }
+}
+public extension UITextView {
+    @IBInspectable var fontFitType: Int {
+        get { return SwiftyFitType.none.rawValue }
+        set {
+            guard let type = SwiftyFitType.init(rawValue: newValue) else { return }
+            guard let xfont = font else { return }
+            self.font =  SwiftyFitsize.shared.fitFontSize(xfont, type: type)
+        }
+    }
+}
+public extension UITextField {
+    @IBInspectable var fontFitType: Int {
+        get { return SwiftyFitType.none.rawValue }
+        set {
+            guard let type = SwiftyFitType.init(rawValue: newValue) else { return }
+            guard let xfont = font else { return }
+            self.font =  SwiftyFitsize.shared.fitFontSize(xfont, type: type)
+        }
+    }
+}
+public extension UIButton {
+    @IBInspectable var fontFitType: Int {
+        get { return SwiftyFitType.none.rawValue }
+        set {
+            guard let type = SwiftyFitType.init(rawValue: newValue) else { return }
+            guard let xfont = titleLabel?.font else { return }
+            self.titleLabel?.font =  SwiftyFitsize.shared.fitFontSize(xfont, type: type)
         }
     }
 }
 
-public extension UILabel {
-    @IBInspectable private(set) var fontFitsize : Bool {
-        get { return true }
-        set { if newValue { font = font~ } }
-    }
-}
 
-public extension UITextView {
-    @IBInspectable private(set) var fontFitsize : Bool {
-        get { return true }
-        set { if newValue, let xfont = font { font = xfont~ } }
-    }
-}
-
-public extension UITextField {
-    @IBInspectable private(set) var fontFitsize : Bool {
-        get { return true }
-        set { if newValue, let xfont = font { font = xfont~ } }
-    }
-}
-
-public extension UIButton {
-    @IBInspectable private(set) var fontFitsize : Bool {
-        get { return true }
-        set { if newValue, let xfont = titleLabel?.font { self.titleLabel?.font = xfont~ } }
-    }
-}
