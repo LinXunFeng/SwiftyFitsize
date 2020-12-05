325201c8aabfb4e0a7e74f8689a8b71bc8dfe7d7
LinXunFeng <598600855@qq.com>
2020-12-05 19:17:55 +0800

    新增高度适配-OC

--- Sources/SwiftyFitsize.h
+++ Sources/SwiftyFitsize.h
@@ -54,5 +54,142 @@ font.sfz
 #define SFZ_EdgeInsets(value) \
 [[SwiftyFitsize sharedSwiftyFitsize] sfz_EdgeInsets:value]
 
+/** ================== ∣ ================== */
+
+#define SF_FH_Font(font) \
+font.sf_fh
+
+#define SF_FH_Int(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sf_fh_int:value]
+
+#define SF_FH_Float(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sf_fh_float:value]
+
+#define SF_FH_Point(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sf_fh_point:value]
+
+#define SF_FH_Size(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sf_fh_size:value]
+
+#define SF_FH_Rect(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sf_fh_rect:value]
+
+#define SF_FH_EdgeInsets(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sf_fh_EdgeInsets:value]
+
+/** ================== ∥ ================== */
+
+#define SFZ_FH_Font(font) \
+font.sfz_fh
+
+#define SFZ_FH_Int(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sfz_fh_int:value]
+
+#define SFZ_FH_Float(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sfz_fh_float:value]
+
+#define SFZ_FH_Point(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sfz_fh_point:value]
+
+#define SFZ_FH_Size(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sfz_fh_size:value]
+
+#define SFZ_FH_Rect(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sfz_fh_rect:value]
+
+#define SFZ_FH_EdgeInsets(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sfz_fh_EdgeInsets:value]
+
+/** ================== ∣= ================== */
+
+#define SF_SCH_Font(font) \
+font.sf_sch
+
+#define SF_SCH_Int(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sf_sch_int:value]
+
+#define SF_SCH_Float(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sf_sch_float:value]
+
+#define SF_SCH_Point(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sf_sch_point:value]
+
+#define SF_SCH_Size(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sf_sch_size:value]
+
+#define SF_SCH_Rect(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sf_sch_rect:value]
+
+#define SF_SCH_EdgeInsets(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sf_sch_EdgeInsets:value]
+
+/** ================== ∥= ================== */
+
+#define SFZ_SCH_Font(font) \
+font.sfz_sch
+
+#define SFZ_SCH_Int(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sfz_sch_int:value]
+
+#define SFZ_SCH_Float(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sfz_sch_float:value]
+
+#define SFZ_SCH_Point(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sfz_sch_point:value]
+
+#define SFZ_SCH_Size(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sfz_sch_size:value]
+
+#define SFZ_SCH_Rect(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sfz_sch_rect:value]
+
+#define SFZ_SCH_EdgeInsets(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sfz_sch_EdgeInsets:value]
+
+/** ================== ∣- ================== */
+
+#define SF_SBH_Font(font) \
+font.sf_sbh
+
+#define SF_SBH_Int(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sf_sbh_int:value]
+
+#define SF_SBH_Float(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sf_sbh_float:value]
+
+#define SF_SBH_Point(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sf_sbh_point:value]
+
+#define SF_SBH_Size(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sf_sbh_size:value]
+
+#define SF_SBH_Rect(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sf_sbh_rect:value]
+
+#define SF_SBH_EdgeInsets(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sf_sbh_EdgeInsets:value]
+
+/** ================== ∥= ================== */
+
+#define SFZ_SBH_Font(font) \
+font.sfz_sbh
+
+#define SFZ_SBH_Int(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sfz_sbh_int:value]
+
+#define SFZ_SBH_Float(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sfz_sbh_float:value]
+
+#define SFZ_SBH_Point(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sfz_sbh_point:value]
+
+#define SFZ_SBH_Size(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sfz_sbh_size:value]
+
+#define SFZ_SBH_Rect(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sfz_sbh_rect:value]
+
+#define SFZ_SBH_EdgeInsets(value) \
+[[SwiftyFitsize sharedSwiftyFitsize] sfz_sbh_EdgeInsets:value]
 
 #endif /* SwiftFitSizeOC_h */
