40d1a9738f7f487d4eff5c8d902a47c438f4d806
LinXunFeng <598600855@qq.com>
2021-08-21 22:39:32 +0800

    update: Example

--- Example/Swift/SwiftyFitsize/ViewController.swift
+++ Example/Swift/SwiftyFitsize/ViewController.swift
@@ -11,6 +11,24 @@ import SwiftyFitsize
 import LXFProtocolTool
 import SnapKit
 
+struct Metric {
+    static let tableViewLeftRightMargin: CGFloat = 10
+    static let tableViewTopMargin: CGFloat = 10
+    static let tableViewHeight: CGFloat = Fit.$width(30) // 去掉左右边距后进行适配的值
+    static let rowViewTopMargin: CGFloat = 5
+    static let rowViewHeight: CGFloat = tableViewHeight - rowViewTopMargin * 2
+    static let rowLeftViewWidth: CGFloat = Fit.$width(177.5)
+    static let rowCenterViewWidth: CGFloat = Fit.$width(100.5)
+    static let rowRightViewWidth: CGFloat = Fit.$width(77)
+}
+
+struct Fit {
+//    @WrappedSwiftyFitsize(fitType: .flexibleWidth, reduceValue: Metric.tableViewLeftRightMargin)
+    @WrappedSwiftyFitsize(reduceValue: Metric.tableViewLeftRightMargin) // fitType 默认值是 .flexibleWidth
+    static var width: CGFloat = 375
+    // 375是初始化值，没有特殊意义，只为调用 Fit.width 时可以取到值，如果用不到 Fit.width 的值可以不进行初始化
+}
+
 class ViewController: UIViewController {
     
     fileprivate let fitLabel : UILabel = {
@@ -28,6 +46,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        /// 移除指定大小后的适配
+        self.testFitWidthForReduceSize()
         
         let portraitBtn = UIButton()
         portraitBtn.setTitle("竖屏后点我", for: .normal)
@@ -59,13 +80,48 @@ class ViewController: UIViewController {
             height: 100)
         redView.frame = redViewFrame~
         
+        let tableView = UIView()
+        tableView.backgroundColor = .darkGray
+        self.view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(Metric.tableViewLeftRightMargin)
+            make.right.equalToSuperview().offset(-Metric.tableViewLeftRightMargin)
+            make.top.equalTo(self.redView.snp.bottom).offset(Metric.tableViewTopMargin)
+            make.height.equalTo(Metric.tableViewHeight)
+        }
+        let rowLeftView = UIView()
+        rowLeftView.backgroundColor = .red
+        tableView.addSubview(rowLeftView)
+        rowLeftView.snp.makeConstraints { make in
+            make.width.equalTo(Metric.rowLeftViewWidth)
+            make.height.equalTo(Metric.rowViewHeight)
+            make.top.equalTo(Metric.rowViewTopMargin)
+            make.left.equalToSuperview()
+        }
+        let rowCenterView = UIView()
+        rowCenterView.backgroundColor = .green
+        tableView.addSubview(rowCenterView)
+        rowCenterView.snp.makeConstraints { make in
+            make.width.equalTo(Metric.rowCenterViewWidth)
+            make.height.top.equalTo(rowLeftView)
+            make.left.equalTo(rowLeftView.snp.right)
+        }
+        let rowRightView = UIView()
+        rowRightView.backgroundColor = .blue
+        tableView.addSubview(rowRightView)
+        rowRightView.snp.makeConstraints { make in
+            make.width.equalTo(Metric.rowRightViewWidth)
+            make.height.top.equalTo(rowLeftView)
+            make.left.equalTo(rowCenterView.snp.right)
+        }
+        
         let fitsizeView = FitsizeView.loadFromNib()
         self.view.addSubview(fitsizeView)
-        fitsizeView.frame = CGRect(x: 0, y: redViewFrame~.maxY, width: 328~, height: 298~)
+        fitsizeView.frame = CGRect(x: 0, y: redViewFrame~.maxY + Metric.tableViewHeight, width: 328~, height: 298~)
         
         portraitBtn.snp.makeConstraints { make in
             make.left.equalTo(redView.snp.right).offset(5)
-            make.height.equalTo(50)
+            make.height.equalTo(40~)
             make.width.equalTo(120)
             make.top.equalTo(redView)
         }
@@ -83,6 +139,34 @@ class ViewController: UIViewController {
     @objc func landscapeBtnClick() {
         let vc = LandscapeViewController()
         self.navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+extension ViewController {
+    /// 移除指定大小后的适配
+    func testFitWidthForReduceSize() {
+        print("默认适配的宽度: \(Fit.width)")
+        
+        // 移除指定尺寸后的适配，调用方式：
+        // 以下都是以适配 tableView 为例，移除 tableView 左右两侧固定的边距，以剩余的宽度来做适配
+        
+        // 方式一：先赋值再取值
+        // 将 20 进行适配
+        Fit.width = 20
+        print("适配后的值 -- \(Fit.width)")
+        
+        // 方式二：使用 $ 将 width 当方法用，传入待适配的值
+        // 将 30 进行适配
+        let aVal = Fit.$width(30)
+        print("适配后的值 aVal -- \(aVal)")
+        
+        // 方式三：调用 SwiftyFitsize.fit 方法
+        let bVal = SwiftyFitsize.fit(
+            size: 40,
+            fitType: .flexibleWidth,
+            reduceValue: Metric.tableViewLeftRightMargin * 2
+        )
+        print("适配后的值 bVal -- \(bVal)")
     }
 }
 
